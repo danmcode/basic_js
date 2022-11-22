ad07ab81e14e94183da9657da944904a32eb24bd
Daniel Alexander Muelas Rivera <desarrollador@seguridadomega.com.co>
2022-11-22 15:51:11 -0500

    Iniciando webpack

---
+++ 03-webpack-inicial/webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpack = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            }
+        ]
+    },
+
+    optimization: {},
+
+    plugins: []
+}
