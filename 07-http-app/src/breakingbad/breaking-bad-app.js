f5643ff834b3c0ce4ef87a93a7fd2525cc3eed19
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2023-08-28 10:29:39 -0500

    http app init

---
+++ 07-http-app/src/breakingbad/breaking-bad-app.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingBadApp = (element) => {
+
+    document.querySelector('#app-title').innerHTML = 'BreakingBad App';
+    element.innerHTML = 'Loading...';
+
+    
+
+}
